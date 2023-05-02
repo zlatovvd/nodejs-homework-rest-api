36809cc0a109097c5d41519a57c744fe96723da0
zlatovvd <zlatovvd@ukr.net>
2023-05-02 21:33:36 +0300

    move DB-HOST toconfig

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const {DB_HOST} = require('./config');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// login - vasyl
+// pass - k8opJ3kpTZ9JbsUb
+// const DB_HOST =
+//   "mongodb+srv://vasyl:k8opJ3kpTZ9JbsUb@cluster0.v95caoh.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
